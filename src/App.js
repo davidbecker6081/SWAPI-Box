e9d8deaf86680f143a6dfcf096649777817c1e60
Dan Alvarez <danalvarez5280@gmail.com>
2017-08-21 19:27:45 -0600

    breaking down how we want to set states and hold our data

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import DataCleaner from './Helpers/DataCleaner'
 class App extends Component {
   constructor() {
     super();
+    this.dataCleaner = new DataCleaner
     this.state = {
-      data: undefined
+      data: this.grabStarWarsData()
     }
+    this.grabStarWarsData = this.grabStarWarsData.bind(this)
   }
 
 //onClick of button, we pass the text as the api we're trying to call and put it into the url
 
 grabStarWarsData() {
-  let newDataObj = new DataCleaner
-  newDataObj.apiCall()
+  // let newDataObj = new DataCleaner
+  this.dataCleaner.apiCall()
   .then(() => {
     this.setState({
-      data: newDataObj
+      data: this.dataCleaner,
     })
   })
   .catch((e) => {
@@ -25,11 +27,19 @@ grabStarWarsData() {
   })
 }
 
+showPeople() {
+  // this.dataCleaner.updatePeopleData()
+  this.setState({
+    peopleArray: this.state.data.peopleData
+  })
+}
+
   render() {
     return (
       <div className="App">
         Hey There Hot Stuff
-        <button onClick={this.grabStarWarsData.bind(this)}>Click ME</button>
+        <button>Click ME</button>
+        <button onClick={this.showPeople.bind(this)}>People</button>
       </div>
     );
   }
